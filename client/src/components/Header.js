413ce04e129682e2011b0c8be03ac319704ab437
Alexander Apostolov <alexanderapostolov@protonmail.com>
2017-09-01 19:07:01 -0400

    Change same domain links from <a> to <Link>. Redirect user to appropriate Landing page depending if they are logged in or not when clicking logo link.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   renderContent() {
+    // this.props.auth to know if user is currently signed in to app
     switch (this.props.auth) {
       case null:
         return;
@@ -25,9 +27,13 @@ class Header extends Component {
     return (
       <nav>
         <div className="nav-wrapper">
-          <a href="" className="left brand-logo">
+          {/* To know what Landing to redirect to, check if "auth" truthy */}
+          <Link
+            to={this.props.auth ? '/surveys' : '/'}
+            className="left brand-logo"
+          >
             Emaily
-          </a>
+          </Link>
           <ul className="right">{this.renderContent()}</ul>
         </div>
       </nav>
